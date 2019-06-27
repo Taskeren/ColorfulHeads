f3d622efd7afabcafd6cf0862bbe53f6efb99e5e
Taskeren <nitu2003@126.com>
2019-06-27 20:34:19 +0800

    [+] 实现了列表重载功能

--- src/main/java/cn/glycol/colorfulheads/ColorfulHeads.java
+++ src/main/java/cn/glycol/colorfulheads/ColorfulHeads.java
@@ -1,19 +1,12 @@
 package cn.glycol.colorfulheads;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.common.collect.Lists;
-
-import cn.glycol.colorfulheads.data.DataManager;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = ColorfulHeads.MODID, name = ColorfulHeads.NAME, version = ColorfulHeads.VERSION)
 public class ColorfulHeads {
@@ -26,6 +19,11 @@ public class ColorfulHeads {
 	@EventHandler
 	public void init(FMLInitializationEvent evt) {
 		SkullTabs.init();
+	}
+	
+	@EventHandler
+	public void server(FMLServerStartingEvent evt) {
+		SkullTabs.reload();
 	}
 	
 }
--- src/main/java/cn/glycol/colorfulheads/SkullTabs.java
+++ src/main/java/cn/glycol/colorfulheads/SkullTabs.java
@@ -2,6 +2,8 @@ package cn.glycol.colorfulheads;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 import cn.glycol.colorfulheads.data.DataManager;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
@@ -11,19 +13,41 @@ import net.minecraft.util.NonNullList;
 
 public class SkullTabs extends CreativeTabs {
 
+	/** 重载时调用的列表 */
+	private static final List<SkullTabs> tabsReloadable = Lists.newArrayList();
+	
 	final ItemStack icon;
 	
-	final List<ItemStack> skulls;
+	protected String dataName;
+	protected List<ItemStack> skulls;
 	
+	/**
+	 * 用这种方法构造的，不会自动读取头颅列表，需要在FMLServerStartingEvent时调用reload()加载。
+	 */
+	public SkullTabs(String name, ItemStack icon, String dataName) {
+		super(name);
+		tabsReloadable.add(this);
+		this.icon = icon;
+		this.dataName = dataName;
+	}
+	
+	/**
+	 * 用这种方法构造的，无法重载头颅列表。
+	 */
+	@Deprecated
 	public SkullTabs(String name, ItemStack icon, List<ItemStack> items) {
 		super(name);
 		this.icon = icon;
 		this.skulls = items;
 	}
 	
-	@Override
-	public ItemStack createIcon() {
-		return icon;
+	/**
+	 * 载入头颅。FMLServerStartingEvent时被调用。
+	 */
+	public void load() {
+		if(dataName != null) {
+			skulls = DataManager.getSkulls(dataName);
+		}
 	}
 	
 	@Override
@@ -32,15 +56,26 @@ public class SkullTabs extends CreativeTabs {
 		super.displayAllRelevantItems(items);
 	}
 	
-	public static SkullTabs tabCustom, tabColors, tabBlocks, tabInterior;
+	@Override
+	public ItemStack createIcon() {
+		return icon;
+	}
 	
+	/** 加载默认的几个列表，请在FMLServerStartingEvent时调用reload()！ */
 	public static void init() {
 		
-		tabCustom = new SkullTabs("cfh.custom", new ItemStack(Items.APPLE), DataManager.getSkulls("custom"));
-		tabColors = new SkullTabs("cfh.colors", new ItemStack(Items.DYE, 1, 15), DataManager.getSkulls("colors"));
-		tabBlocks = new SkullTabs("cfh.blocks", new ItemStack(Blocks.BRICK_BLOCK), DataManager.getSkulls("blocks"));
-		tabInterior = new SkullTabs("cfh.interior", new ItemStack(Items.FLOWER_POT), DataManager.getSkulls("interior"));
+		new SkullTabs("cfh.custom", new ItemStack(Items.APPLE), "custom");
+		new SkullTabs("cfh.colors", new ItemStack(Items.DYE, 1, 15), "colors");
+		new SkullTabs("cfh.blocks", new ItemStack(Blocks.BRICK_BLOCK), "blocks");
+		new SkullTabs("cfh.interior", new ItemStack(Items.FLOWER_POT), "interior");
 		
+	}
+	
+	/**
+	 * 调用可重载头颅的所有列表重载头颅。请在FMLServerStartingEvent调用！
+	 */
+	public static void reload() {
+		tabsReloadable.forEach(tab -> tab.load());
 	}
 	
 }
